1fe57f8e9a006208886480637112c38983a374c5
camperfail86 <mega.kochkin@inbox.ru>
2022-10-25 15:08:22 +0500

    1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 import './download.js';
 
 // Код для следующего дз
-const delEte = document.querySelector('.img-upload__overlay');
+const uploadFile = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const closeButton = uploadFile.querySelector('#upload-cancel');
 
+// Нужно сделать при загрузке файла этот пункт
 document.addEventListener('keydown', () => {
-  delEte.classList.remove('hidden');
+  uploadFile.classList.remove('hidden');
+  body.classList.add('modal-open');
 });
+
+closeButton.addEventListener('click', () => {
+  uploadFile.classList.add('hidden');
+  body.classList.remove('modal-open');
+});
+
+
